b5ae1670a0c7cf3935526d6584babea895445d97
Simon Wietrich <simon.wietrich@gmail.com>
2016-05-07 16:21:38 +0100

    Add next bus display

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var config = require('./config');
 var bot = new TelegramBot(config.token, {polling: true});
 
 var room_env = {};
+var next_bus = {};
 
 // Setup MQTT Client
 var mqttClient = mqtt.connect('mqtt://127.0.0.1', {port: 1883});
@@ -33,13 +34,20 @@ bot.onText(/\/start/, function (msg, match) {
 
 bot.onText(/Temperature/, function(msg, match) {
   var fromId = msg.from.id;
-  bot.sendMessage(fromId, room_env.temperature + '°C');
+  bot.sendMessage(fromId, 'It is' + room_env.temperature + '°C inside.\nThe luminosity is ' + room_env.luminosity + ' Lux');
+});
+
+bot.onText(/Bus/, function(msg, match) {
+  var fromId = msg.from.id;
+  bot.sendMessage(fromId, 'The next bus is a ' + next_bus.route + ' in ' + next_bus.duetime + ' min');
 });
 
 mqttClient.on('message', function(topic, message) {
   console.log(topic);
   if (topic === 'local-env') {
     room_env = JSON.parse(message.toString('ascii'));
+  } else if (topic === 'bus-monitor') {
+    next_bus = JSON.parse(message.toString('ascii'));
   }
 })
 
