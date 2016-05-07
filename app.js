d2412ccc288fccb42379f6a84c4277fee6db850c
Simon Wietrich <simon.wietrich@gmail.com>
2016-05-07 15:17:47 +0100

    Add config file

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var TelegramBot = require('node-telegram-bot-api');
+var mqtt = require('mqtt');
+var config = require('./config');
 
-var token = '223485578:AAFGD2p5a6lbkLZz5JhZh6Kip6gDTRKzI_c';
-var my_id = '193931395';
-// Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+// Setup bot
+var bot = new TelegramBot(config.token, {polling: true});
 
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  console.log(msg);
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
-});
+// Setup MQTT Client
+var mqttClient = mqtt.connect('mqtt://127.0.0.1', {port: 1883});
 
 // Matches /start[whatever]
 bot.onText(/\/start/, function (msg, match) {
@@ -33,8 +28,9 @@ bot.onText(/\/start/, function (msg, match) {
 });
 
 bot.onText(/Temperature/, function(msg, match) {
-  if (msg.from.id === my_id) {
-    
+  // Check if the user is me
+  if (msg.from.id === config.chat_id) {
+    console.log('hello');
   }
 });
 
